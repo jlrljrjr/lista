61a04a71a006564bf0714f199e956b4d1fcc48c9
Jorge Robles Jr <jorgeluis.robleslopez@umg.edu.mx>
2024-02-15 22:04:13 -0600

    Commit inicial

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+const data =[
+    {
+        id:1,
+        nombre:"Jorge",
+        carrera: "ciber",
+        semestre:4
+    },
+    {
+        id:2,
+        nombre:"Mario",
+        carrera: "ciber",
+        semestre:4
+    },
+    {
+        id:3,
+        nombre:"Vale",
+        carrera: "ciber",
+        semestre:5
+    },
+]
+
+app.get("/",(req,res)=>{
+    res.send("Hola mundo") 
+
+})
+
+app.get("/data/all",(req,res)=>{
+    res.status(200).json(data)
+
+})
+app.get("/data",(req,res)=>{
+    const query_semestre=req.query.semestre
+    const query_carrera=req.query.carrera
+    if(query_semestre&&query_carrera){
+            const filtro= data.filter(item=>item.semestre==query_semestre&&item.carrera==query)
+            if(filtro>0){
+                res.status(200).json(filtro)
+            }else{
+                res.status(404).json({mensaje:"no encontrado"})
+            }
+    }else{
+        res.status(301).redirect("/data/all")
+    }
+
+})
+app.get("/data/:id",(req,res)=>{
+    const id_user=req.params.id
+    const encontrado = data.find(item=>item.id==id_user)
+    if(encontrado){
+        res.status(200).json(encontrado)
+    }
+    else{
+        res.status(404).json({message:"no encontrado"})
+    }
+})
+
+app.post("/data",(req,res)=>{
+    const user_body=req.body
+    data.push(user_body)
+    res.status(201).json(data)
+})
+
+app.put("/data/:id",(req,res)=>{
+    const user_body=req.body
+    const param=req.params.id
+    const encontrado=data.findIndex(item=>item.id==param)
+    if(encontrado!=-1){
+        data[encontrado]=user_body
+        res.status(201).json(data)
+    }else{
+        res.status(404).json({message:"no encontrado"})
+    }
+})
+
+app.listen(port,()=>{
+    console.log("escuchando el puerto: ",port)
+})
